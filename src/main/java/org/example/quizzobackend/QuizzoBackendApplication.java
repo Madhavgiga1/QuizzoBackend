51c08a57c95110ede2ff00fe33975c7c281a8642
Madhav <sharmamadhav208@gmail.com>
2025-09-25 23:43:14 +0530

    first commit

---
+++ src/main/java/org/example/quizzobackend/QuizzoBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.quizzobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizzoBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/quizzobackend/QuizzoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quizzobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
